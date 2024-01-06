736ef8576e5138343ad314c6dbbea0af449a312c
Koushik <koushiknus@gmail.com>
2024-01-05 19:30:21 -0600

    New : Sample Functionalities added for Test

---
+++ ArchitecturePlayground/app/src/main/java/com/example/architectureplayground/Person.kt
@@ -0,0 +1,8 @@
+package com.example.architectureplayground
+
+data class Person(
+    val firstName : String,
+    val lastName : String,
+    var nationality : String,
+    var age : Int
+)
---
+++ ArchitecturePlayground/app/src/test/java/com/example/architectureplayground/MainViewModelTest.kt
@@ -0,0 +1,7 @@
+package com.example.architectureplayground
+
+
+
+class MainViewModelTest{
+
+}
